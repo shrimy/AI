dfbd18b45736a710c2fde7bb57824f4a8b43c27c
shrimy <prybka2@gmail.com>
2025-04-14 19:30:43 +0200

    Zmieniono bazę danych na PostgreSQL

--- przychodnia_app/settings.py
+++ przychodnia_app/settings.py
@@ -77,10 +77,22 @@ WSGI_APPLICATION = 'przychodnia_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Przychodnia',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'PORT': '5432',
+        'HOST': 'localhost',
+
     }
 }
 
